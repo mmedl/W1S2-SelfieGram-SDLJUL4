e56081de2cff18d5129374b9a3cf3e753eb491fd
Sheila Dillon-Leitch <sdillonleitch@apple.com>
2018-07-04 19:11:10 -0700

    Initial Commit

---
+++ W1S2-SelfieGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  W1S2-SelfieGram
+//
+//  Created by Sheila Dillon-Leitch on 2018-07-04.
+//  Copyright © 2018 Sheila Dillon-Leitch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
